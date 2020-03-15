92bfa5cf4056a0c5d22b4b95681b05cc9aa1a5a6
Braden Lawrence <b@bradenlawrence.com>
2020-03-14 20:03:42 -0400

    export a new ADD_GROCERY action

--- src/modules/groceries.js
+++ src/modules/groceries.js
@@ -3,13 +3,28 @@ const initialState = {
   name: ''
 }
 
+const ADD_GROCERY = "ADD_GROCERY"
+const addNewGrocery = (grocery) => {
+  return {
+    type: ADD_GROCERY,
+    grocery
+  }
+}
+
 const groceries = (state = initialState, action) => {
   switch(action.type) {
+    case ADD_GROCERY:
+      const updatedGroceries = state.groceryList.concat(action.grocery)
+      return {
+        ...state,
+        groceryList: updatedGroceries
+      }
     default:
       return state
   }
 }
 
 export {
+  addNewGrocery,
   groceries
 }
